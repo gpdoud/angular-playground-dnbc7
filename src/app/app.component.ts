20dd175499035901530e00b76127f22ab3eef4a6
Greg Doud <gpdoud@gmail.com>
2019-10-15 16:11:46 -0400

    "initial commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'playground';
+  message = "Angular is cool!";
+  value: string = "Initial value";
+
+  clicked() {
+    this.message = "Angular is very cool!";
+  }
 }
